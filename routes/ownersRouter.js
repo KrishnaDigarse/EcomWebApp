ea7fa01787a6ea9bb421a8657f27178f5962d0ee
Krishna <krishnadigarse2004@gmail.com>
2025-05-04 19:40:08 +0530

    Owner route setup

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,30 @@
 const express = require("express")
 const router = express.Router()
+const ownerModel = require("../models/owners-model")
+const ownersModel = require("../models/owners-model")
 
 router.get("/", function(req, res){
     res.send("This is Owners Route.")
 })
+
+if(process.env.NODE_ENV === "development"){
+    router.post("/create", async (req, res) => {
+        let owner = await ownerModel.find()
+        if(owner.length > 0){
+            return res
+                .status(502)
+                .send("You do not have permission to be an owner.")
+        }
+
+        let {fullname, email, password} = req.body
+
+        let createdOwner = await ownerModel.create({
+            fullname,
+            email,
+            password,
+        })
+        res.status(201).send(createdOwner)
+    })
+}
 
 module.exports = router
